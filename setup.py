708329239d152dce5526e5334661e4c2fc9ec83a
Harshal Kumre <kumreharshalkumar@gmail.com>
2023-02-20 23:08:29 +0530

    setup file completed.

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import find_packages, setup
+from typing import List
+
+# Declaring variables for setup functions
+PROJECT_NAME = "medical-insurance-premium-prediction"
+VERSION = "0.0.4"
+AUTHOR = "Harshal Kumre"
+DESCRIPTION = "This is a simple project for industry ready solution"
+REQUIREMENT_FILE_NAME = "requirements.txt"
+
+HYPHEN_E_DOT = "-e ."
+
+def get_requirements_list(file_path=REQUIREMENT_FILE_NAME) -> List[str]:
+    """
+    Description: This function is going to return list of requirement
+    mention in requirements.txt file
+    return This function is going to return a list which contain name
+    of libraries mentioned in requirements.txt file
+    """
+    with open(file_path) as requirement_file:
+        requirement_list = requirement_file.readlines()
+        requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_file]
+        if HYPHEN_E_DOT in requirement_list:
+            requirement_list.remove(HYPHEN_E_DOT)
+        return requirement_list
+
+
+setup(
+    name=PROJECT_NAME,
+    version=VERSION,
+    author=AUTHOR,
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=get_requirements_list()
+)
