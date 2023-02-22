d4124fd7089d6b87d76bfe8c1eef43eee87daefd
Harshal Kumre <kumreharshalkumar@gmail.com>
2023-02-22 15:11:13 +0530

    data validation

--- insurance/entity/config_entity.py
+++ insurance/entity/config_entity.py
@@ -11,7 +11,7 @@ TEST_FILE_NAME = "test.csv"
 class TrainingPipelineConfig:
     def __init__(self):
         try:
-            self.artifacts_dir = os.path.join(os.getcwd(), "artifact", f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}")
+            self.artifact_dir = os.path.join(os.getcwd(), "artifact", f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}")
         except Exception as e:
             raise InsuranceException(e,sys)
 
@@ -20,7 +20,7 @@ class DataIngestionConfig:
         try:
             self.database_name = "INSURANCE"
             self.collection_name = "INSURANCE_PROJECT"
-            self.data_ingestion_dir = os.path.join(training_pipeline_config.artifacts_dir, "data_ingestion")
+            self.data_ingestion_dir = os.path.join(training_pipeline_config.artifact_dir, "data_ingestion")
             self.feature_store_file_path = os.path.join(self.data_ingestion_dir, "feature_store", FILE_NAME)
             self.train_file_path = os.path.join(self.data_ingestion_dir, "dataset", TRAIN_FILE_NAME)
             self.test_file_path = os.path.join(self.data_ingestion_dir, "dataset", TEST_FILE_NAME)
@@ -38,9 +38,11 @@ class DataIngestionConfig:
 
 
 class DataValidationConfig:
-    def __init(self, training_pipeline_config: TraningPipelineConfig):
+    def __init__(self, training_pipeline_config: TrainingPipelineConfig):
         try:
             self.data_validation_dir = os.path.join(training_pipeline_config.artifact_dir, "data_validation")
             self.report_file_path = os.path.join(self.data_validation_dir, "report.yaml") # yaml, json, csv
+            self.missing_threshold:float = 0.2
+            self.base_file_path = os.path.join("insurance.csv") 
         except Exception as e:
             raise InsuranceException(e, sys)
