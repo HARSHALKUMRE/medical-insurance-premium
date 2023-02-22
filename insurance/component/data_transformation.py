4584135012c3a221dec7711f0500b51472f8c928
Harshal Kumre <kumreharshalkumar@gmail.com>
2023-02-22 22:56:06 +0530

    data transformation

--- insurance/component/data_transformation.py
+++ insurance/component/data_transformation.py
@@ -1,10 +1,16 @@
 import os, sys
+import pandas as pd
+import numpy as np
 from insurance.exception import InsuranceException
 from insurance.logger import logging
 from insurance.entity import artifact_entity, config_entity
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import RobustScaler
 from sklearn.pipeline import Pipeline
+from insurance.configuration import TARGET_COLUMN
+from sklearn.preprocessing import LabelEncoder
+#from sklearn.imblearn import SMOTETomek
+from insurance import utils
 
 # Missing value imputation
 # outlier handling
@@ -16,7 +22,7 @@ class DataTransformation:
     def __init__(self, data_transformation_config: config_entity.DataTransformationConfig,
     data_ingestion_artifact: artifact_entity.DataIngestionArtifact):
         try:
-            logging.info(f"{'=' * 20} Data Transformation {'=' * 20}")
+            logging.info(f"{'=' * 20} Data transformation log started {'=' * 20}")
             self.data_transformation_config = data_transformation_config
             self.data_ingestion_artifact = data_ingestion_artifact
         except Exception as e:
@@ -39,6 +45,80 @@ class DataTransformation:
 
     def initiate_data_transformation(self,) -> artifact_entity.DataTransformationArtifact:
         try:
-            pass
+            # Reading training and testing files
+            train_df = pd.read_csv(self.data_ingestion_artifact.train_file_path)
+            test_df = pd.read_csv(self.data_ingestion_artifact.test_file_path)
+
+            # Selecting input feature for train and test dataframe
+            input_feature_train_df = train_df.drop(TARGET_COLUMN, axis=1)
+            input_feature_test_df = test_df.drop(TARGET_COLUMN, axis=1)
+
+            # Selecting target feature for train and test dataframe
+            target_feature_train_df = train_df[TARGET_COLUMN]
+            target_feature_test_df = test_df[TARGET_COLUMN]
+            
+            label_encoder = LabelEncoder()
+            # label_encoder.fit(target_feature_train_df)
+
+            # Transformation on target column
+            target_feature_train_arr = target_feature_train_df.squeeze()
+            target_feature_test_arr = target_feature_test_df.squeeze()
+
+            # Transformation on categorical column
+            for col in input_feature_train_df.columns:
+                if input_feature_train_df[col].dtypes == 'O':
+                    input_feature_train_df[col] = label_encoder.fit_transform(input_feature_train_df[col])
+                    input_feature_test_df[col] = label_encoder.fit_transform(input_feature_test_df[col])
+                else:
+                    input_feature_train_df[col] = input_feature_train_df[col]
+                    input_feature_test_df[col] = input_feature_test_df[col]
+
+            
+            transformation_pipeline = DataTransformation.get_data_transformer_object()
+            transformation_pipeline.fit(input_feature_train_df)
+
+            # Transforming input features
+            input_feature_train_arr = transformation_pipeline.transform(input_feature_train_df)
+            input_feature_test_arr = transformation_pipeline.transform(input_feature_test_df)
+
+            # smt = SMOTETomek(random_state=42)
+            # logging.info(f"Before resampling in training set Input: {input_feature_train_arr.shape} Target:{target_feature_train_arr.shape}")
+            # input_feature_train_arr, target_feature_train_arr = smt.fit_resample(input_feature_train_arr, target_feature_train_arr)
+            # logging.info(f"After resampling in training set Input: {input_feature_train_arr.shape} Target:{target_feature_train_arr.shape}")
+            
+            # logging.info(f"Before resampling in testing set Input: {input_feature_test_arr.shape} Target:{target_feature_test_arr.shape}")
+            # input_feature_test_arr, target_feature_test_arr = smt.fit_resample(input_feature_test_arr, target_feature_test_arr)
+            # logging.info(f"After resampling in testing set Input: {input_feature_test_arr.shape} Target:{target_feature_test_arr.shape}")
+            
+            # target encoder
+            train_arr = np.c_[input_feature_train_arr, target_feature_train_arr]
+            test_arr = np.c_[input_feature_test_arr, target_feature_test_arr]
+
+            # save numpy array
+            utils.save_numpy_array_data(file_path = self.data_transformation_config.transformed_train_path,
+            array=train_arr)
+
+            utils.save_numpy_array_data(file_path = self.data_transformation_config.transformed_test_path,
+            array=test_arr)
+
+            utils.save_object(file_path = self.data_transformation_config.transform_object_path, 
+            obj = transformation_pipeline)
+
+            utils.save_object(file_path = self.data_transformation_config.target_encoder_path,
+            obj = label_encoder)
+            
+
+
+            # Creating data transformation artifact
+            data_transformation_artifact = artifact_entity.DataTransformationArtifact(
+                transform_object_path = self.data_transformation_config.transform_object_path,
+                transformed_train_path = self.data_transformation_config.transformed_train_path,
+                transformed_test_path = self.data_transformation_config.transformed_test_path,
+                target_encoder_path = self.data_transformation_config.target_encoder_path    
+            )
+            logging.info(f"Data Transformation object {data_transformation_artifact}")
+            return data_transformation_artifact
+  
         except Exception as e:
             raise InsuranceException(e, sys)
+
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from insurance.entity.config_entity import DataIngestionConfig
 from insurance.entity import config_entity
 from insurance.component.data_ingestion import DataIngestion
 from insurance.component.data_validation import DataValidation
+from insurance.component.data_transformation import DataTransformation
 
 #def test_logger_and_exception():
     #try:
@@ -34,6 +35,12 @@ if __name__ == "__main__":
         data_ingestion_artifact=data_ingestion_artifact)
 
         data_validation_artifact = data_validation.initiate_data_validation()
+
+        # data transformation
+        data_transformation_config = config_entity.DataTransformationConfig(training_pipeline_config=training_pipeline_config)
+        data_transformation = DataTransformation(data_transformation_config=data_transformation_config,
+        data_ingestion_artifact=data_ingestion_artifact)
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
 
     except Exception as e:
         print(e)
